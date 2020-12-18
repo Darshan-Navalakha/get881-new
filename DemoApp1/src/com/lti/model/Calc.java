45492f06919153862b677314a65c515b86a1af27
Darshan-Navalakha <darshannavalakha.04@gmail.com>
2020-12-18 16:28:16 +0530

    modified add method

--- DemoApp1/src/com/lti/model/Calc.java
+++ DemoApp1/src/com/lti/model/Calc.java
@@ -3,7 +3,7 @@ package com.lti.model;
 public class Calc {
 
 	public void add(int num1, int num2) {
-		System.out.println("Addition:" +(num1+num2));
+		System.out.println("sum:" +num1+num2);
 	}
 	public void product(int num1,int num2) {
 		System.out.println("multiply:" +(num1*num2));
